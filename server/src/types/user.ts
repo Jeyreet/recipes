e8c08f2cea7f4f90d59b53d564818230fb1046b2
Jeyreet <jeyreet@yandex.ru>
2025-11-03 18:29:11 +0700

    feat: add regex validation for login and password fields in user schema

--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -8,8 +8,8 @@ type User = BaseRow<{
 }>
 
 const user = {
-  login: z.string().min(4).max(40),
-  password: z.string().min(4).max(100),
+  login: z.string().min(4).max(40).regex(/^[A-Za-z0-9]+$/),
+  password: z.string().min(4).max(100).regex(/^[^\sА-Яа-яЁё]+$/),
   name: z.string().min(4).max(40),
 }
 
